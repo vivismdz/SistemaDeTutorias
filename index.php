b99a9f85c9841e59c92cb7f779eb0975fc86dea3
vivismdz <vivianamengar@hotmail.com>
2013-11-26 23:07:08 -0600

    index y registro
    index y registro para que hagas esa parte Miguel, este trae acentos
    corregidos

--- index.php
+++ index.php
@@ -25,13 +25,7 @@
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 
-        <!-- Favicon and touch icons -->
-         <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    
        
 
     </head>
@@ -50,29 +44,15 @@
                                 <span class="icon-bar"></span>
                             </a>
                             <h1>
-                                <a class="brand" href="index.html"></a>
+                                <a class="brand" href="index.php"></a>
                             </h1>
 
                             <div class="nav-collapse collapse">
                                 <ul class="nav pull-right">
                                     <li class="current-page">
-                                        <a href="index.html"><i class="icon-home"></i><br />Inicio</a>
+                                        <a href="index.php"><i class="icon-home"></i><br />Inicio</a>
                                     </li>
-                                    <!--
-                                    <li>
-                                        <a href="portfolio.html"><i class="icon-camera"></i><br />Portfolio</a>
-                                    </li>
-                                    -->
-                                    <!--
-                                    <li>
-                                        <a href="#"><i class="icon-comments"></i><br />Blog</a>
-                                    </li>
-                                    -->
-                                    <!--
-                                    <li>
-                                        <a href="services.html"><i class="icon-tasks"></i><br />Services</a>
-                                    </li>
-                                    -->
+                              
                                     <li>
                                         <a href="about.html"><i class="icon-user"></i><br />Acerca de</a>
                                     </li>
@@ -87,36 +67,7 @@
             </div>
         </div>
 
-        <!-- Slider 
-        <div class="slider">
-            <div class="container">
-                <div class="row">
-                    <div class="span10 offset1">
-                        <div class="flexslider">
-                            <ul class="slides">
-                                <li data-thumb="assets/img/slider/1.jpg">
-                                    <img src="assets/img/slider/1.jpg">
-                                    <p class="flex-caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur.</p>
-                                </li>
-                                <li data-thumb="assets/img/slider/2.jpg">
-                                    <img src="assets/img/slider/2.jpg">
-                                    <p class="flex-caption">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                </li>
-                                <li data-thumb="assets/img/slider/5.jpg">
-                                    <img src="assets/img/slider/5.jpg">
-                                    <p class="flex-caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur.</p>
-                                </li>
-                                <li data-thumb="assets/img/slider/6.jpg">
-                                    <img src="assets/img/slider/6.jpg">
-                                    <p class="flex-caption">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                </li
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        /Slider -->
+
 
 
         <!-- Services -->
@@ -141,162 +92,26 @@
                                 </form>
                                 <center><a href="registro.php">Registrarse</a></center>
                             </div>
-                            <!--
-                            <div class="contact-address span5">
-                                <h4>We Are Here</h4>
-                                <div class="map"></div>
-                                <h4>Address</h4>
-                                <p>Via Principe Amedeo 9 <br> 10100, Torino, TO, Italy</p>
-                                <p>Phone: 0039 333 12 68 347</p>
-                            </div>
-                            -->
+                           
                         </div>
                     </div>
                 </div>
-                <!--
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-table"></i>
-                    </div>
-                    <h4>Responsive Layout</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-magic"></i>
-                    </div>
-                    <h4>Awesome Logos</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-print"></i>
-                    </div>
-                    <h4>High Res Prints</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                -->
+          
             </div>
         </div>
 
-       
-
-        <!-- Latest Work 
-        <div class="portfolio container">
-            <div class="portfolio-title">
-                <h3>Our Latest Work</h3>
-            </div>
-            <div class="row">
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work1.jpg" alt="">
-                    <h4>Lorem Website</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work1.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work2.jpg" alt="">
-                    <h4>Ipsum Logo</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work2.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work3.jpg" alt="">
-                    <h4>Dolor Prints</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work3.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work4.jpg" alt="">
-                    <h4>Sit Amet Website</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work4.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        /Latest Work -->
-
-
-        <!-- Testimonials 
-        <div class="testimonials container">
-            <div class="testimonials-title">
-                <h3>Testimonials</h3>
-            </div>
-            <div class="row">
-                <div class="testimonial-list span12">
-                    <div class="tabbable tabs-below">
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="A">
-                                <img src="assets/img/testimonials/1.jpg" title="" alt="">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
-                            </div>
-                            <div class="tab-pane" id="B">
-                                <img src="assets/img/testimonials/2.jpg" title="" alt="">
-                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
-                            </div>
-                            <div class="tab-pane" id="C">
-                                <img src="assets/img/testimonials/3.jpg" title="" alt="">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
-                            </div>
-                            <div class="tab-pane" id="D">
-                                <img src="assets/img/testimonials/1.jpg" title="" alt="">
-                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
-                            </div>
-                        </div>
-                       <ul class="nav nav-tabs">
-                           <li class="active"><a href="#A" data-toggle="tab"></a></li>
-                           <li class=""><a href="#B" data-toggle="tab"></a></li>
-                           <li class=""><a href="#C" data-toggle="tab"></a></li>
-                           <li class=""><a href="#D" data-toggle="tab"></a></li>
-                       </ul>
-                   </div>
-                </div>
-            </div>
-        </div>
-       /Testimonials -->
 
         <!-- Site Description -->
         <div class="presentation container">
-            <h2><span class="violet">Sistema de Tutorias</span>.</h2>
-            <p>Universidad Politecnica de San Luis Potosi</p>
+            <h2><span class="violet">Sistema de Tutorías</span>.</h2>
+            <p>Universidad Politécnica de San Luis Potosí</p>
         </div>
 
        <br><br>
         <!-- Footer -->
         <footer>
             <div class="container">
-               <!-- <div class="row">
-                    <div class="widget span4">
-                        <h4>Acerca de Nosotros</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-                        <p><a href="">Read more...</a></p>
-                    </div>
-                    <div class="widget span4">
-                        <h4>Ultimos Tweets</h4>
-                        <div class="show-tweets"></div>
-                    </div>
-                   
-                    <div class="widget span4">
-                        <h4>Contactanos</h4>
-                        <p><i class="icon-map-marker"></i> Address: Urbano Villalon #500</p>
-                        <p><i class="icon-phone"></i> Phone: </p>
-                        <p><i class="icon-envelope-alt"></i> Email: <a href="">contact@sistematutoriasupslp.edu.mx</a></p>
-                    </div>
-                </div>-->
+             
                 <div class="footer-border"></div>
                 <div class="row">
                     <div class="copyright span4">
--- registro.php
+++ registro.php
@@ -25,12 +25,7 @@
         <![endif]-->
 
         <!-- Favicon and touch icons -->
-         <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+      
        
 
     </head>
@@ -49,13 +44,13 @@
                                 <span class="icon-bar"></span>
                             </a>
                             <h1>
-                                <a class="brand" href="index.html"></a>
+                                <a class="brand" href="index.php"></a>
                             </h1>
 
                             <div class="nav-collapse collapse">
                                 <ul class="nav pull-right">
                                     <li class="current-page">
-                                        <a href="index.html"><i class="icon-home"></i><br />Inicio</a>
+                                        <a href="index.php"><i class="icon-home"></i><br />Inicio</a>
                                     </li>
                                
                                     <li>
@@ -95,162 +90,27 @@
                                     <button type="submit">Enviar</button><br><button type="submit">Registrarse</button>
                                 </form>
                             </div>
-                            <!--
-                            <div class="contact-address span5">
-                                <h4>We Are Here</h4>
-                                <div class="map"></div>
-                                <h4>Address</h4>
-                                <p>Via Principe Amedeo 9 <br> 10100, Torino, TO, Italy</p>
-                                <p>Phone: 0039 333 12 68 347</p>
-                            </div>
-                            -->
+                         
                         </div>
                     </div>
                 </div>
-                <!--
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-table"></i>
-                    </div>
-                    <h4>Responsive Layout</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-magic"></i>
-                    </div>
-                    <h4>Awesome Logos</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                <div class="service span3">
-                    <div class="icon-awesome">
-                        <i class="icon-print"></i>
-                    </div>
-                    <h4>High Res Prints</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
-                    <a href="services.html">Read more</a>
-                </div>
-                -->
+      
             </div>
         </div>
 
-       
-
-        <!-- Latest Work 
-        <div class="portfolio container">
-            <div class="portfolio-title">
-                <h3>Our Latest Work</h3>
-            </div>
-            <div class="row">
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work1.jpg" alt="">
-                    <h4>Lorem Website</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work1.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work2.jpg" alt="">
-                    <h4>Ipsum Logo</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work2.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work3.jpg" alt="">
-                    <h4>Dolor Prints</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work3.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-                <div class="work span3">
-                    <img src="assets/img/portfolio/work4.jpg" alt="">
-                    <h4>Sit Amet Website</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-                    <div class="icon-awesome">
-                        <a href="assets/img/portfolio/work4.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
-                        <a href="portfolio.html"><i class="icon-link"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        /Latest Work -->
-
-
-        <!-- Testimonials 
-        <div class="testimonials container">
-            <div class="testimonials-title">
-                <h3>Testimonials</h3>
-            </div>
-            <div class="row">
-                <div class="testimonial-list span12">
-                    <div class="tabbable tabs-below">
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="A">
-                                <img src="assets/img/testimonials/1.jpg" title="" alt="">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
-                            </div>
-                            <div class="tab-pane" id="B">
-                                <img src="assets/img/testimonials/2.jpg" title="" alt="">
-                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
-                            </div>
-                            <div class="tab-pane" id="C">
-                                <img src="assets/img/testimonials/3.jpg" title="" alt="">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
-                            </div>
-                            <div class="tab-pane" id="D">
-                                <img src="assets/img/testimonials/1.jpg" title="" alt="">
-                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
-                            </div>
-                        </div>
-                       <ul class="nav nav-tabs">
-                           <li class="active"><a href="#A" data-toggle="tab"></a></li>
-                           <li class=""><a href="#B" data-toggle="tab"></a></li>
-                           <li class=""><a href="#C" data-toggle="tab"></a></li>
-                           <li class=""><a href="#D" data-toggle="tab"></a></li>
-                       </ul>
-                   </div>
-                </div>
-            </div>
-        </div>
-       /Testimonials -->
+ 
 
         <!-- Site Description -->
         <div class="presentation container">
-            <h2><span class="violet">Sistema de Tutorias</span>.</h2>
-            <p>Universidad Politecnica de San Luis Potosi</p>
+            <h2><span class="violet">Sistema de Tutorías</span>.</h2>
+            <p>Universidad Politécnica de San Luis Potosí</p>
         </div>
 
        <br><br>
         <!-- Footer -->
         <footer>
             <div class="container">
-               <!-- <div class="row">
-                    <div class="widget span4">
-                        <h4>Acerca de Nosotros</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-                        <p><a href="">Read more...</a></p>
-                    </div>
-                    <div class="widget span4">
-                        <h4>Ultimos Tweets</h4>
-                        <div class="show-tweets"></div>
-                    </div>
-                   
-                    <div class="widget span4">
-                        <h4>Contactanos</h4>
-                        <p><i class="icon-map-marker"></i> Address: Urbano Villalon #500</p>
-                        <p><i class="icon-phone"></i> Phone: </p>
-                        <p><i class="icon-envelope-alt"></i> Email: <a href="">contact@sistematutoriasupslp.edu.mx</a></p>
-                    </div>
-                </div>-->
+             
                 <div class="footer-border"></div>
                 <div class="row">
                     <div class="copyright span4">
